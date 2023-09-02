ac92122601278f6c73a163064c685e58e3f65c2a
DimaBoretskiy <137459536+DimaBoretskiy@users.noreply.github.com>
2023-09-02 21:32:36 +0300

    init angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'dashboard';
+  announcements: any[] = Array(20) 
+}
