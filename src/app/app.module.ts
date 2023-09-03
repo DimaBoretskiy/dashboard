08f0bb23d16d209db77bedaf2b8f9dcfcbee5ab7
DimaBoretskiy <137459536+DimaBoretskiy@users.noreply.github.com>
2023-09-03 20:19:54 +0300

    delete unused component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { ComponentsComponent } from './components/components.component';
 import { ProductItemComponent } from './components/product-item/product-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ComponentsComponent,
     ProductItemComponent,
   ],
   imports: [
