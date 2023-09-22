1227b9f229bf3dfeed320a24756c4d119823ac28
DimaBoretskiy <137459536+DimaBoretskiy@users.noreply.github.com>
2023-09-22 19:08:56 +0300

    fix product item

--- src/app/modules/my-announcement/my-announcement.module.ts
+++ src/app/modules/my-announcement/my-announcement.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyAnnouncementComponent } from './pages/my-announcement/my-announcement.component';
 import { MyAnnouncementRoutingModule } from './my-announcement.routing';
-import { ProductItemComponent } from 'src/app/shared/components/product-item/product-item.component';
+import { ProductsItemComponentModule } from 'src/app/shared/components/product-item/product-item.component';
 
 
 
 @NgModule({
   declarations: [
     MyAnnouncementComponent,
-    ProductItemComponent
   ],
   imports: [
     CommonModule,
-    MyAnnouncementRoutingModule
+    MyAnnouncementRoutingModule,
+    ProductsItemComponentModule
   ]
 })
 export class MyAnnouncementModule { }
