7f355be22302e2950d9f64e9e9587894f6544516
DimaBoretskiy <137459536+DimaBoretskiy@users.noreply.github.com>
2023-10-08 16:44:01 +0300

    add images service

--- src/app/shared/components/product-item/product-item.component.ts
+++ src/app/shared/components/product-item/product-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, NgModule } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, NgModule, OnInit } from '@angular/core';
+import { ImagesService } from '@services/images/images.service';
 import { IAnnouncement } from 'src/app/core/models/announcement.model';
 
 @Component({
@@ -6,8 +8,16 @@ import { IAnnouncement } from 'src/app/core/models/announcement.model';
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.scss']
 })
-export class ProductItemComponent {
+export class ProductItemComponent implements OnInit {
   @Input() announcement: IAnnouncement | null = null;
+  image!: string | null 
+  constructor (private _imageService: ImagesService){
+    
+  }
+  ngOnInit(): void {
+    this.image = this.announcement?.imagesIds[0] ?  this._imageService.getImageUrl(this.announcement?.imagesIds[0]) : null
+  }
+ 
 }
 
 @NgModule({
